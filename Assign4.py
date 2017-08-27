a5e823cdcb040ff801f6cf62033ce599bfb18ddd
pralhad30 <31305550+pralhad30@users.noreply.github.com>
2017-08-27 23:40:05 +1000

    Update Assign4.py
    Added Latest Script for Assignment 4

--- Assign4.py
+++ Assign4.py
@@ -1,32 +1,7 @@
+X = [('Tom',19,80),('John',20,90),('
+Jony',17,91),('
+Jony',17,93),('
+Json',21,85)]
+Y = sorted(X,key=lamda tup:(tup[0],tup[1],tup[2]))
 
-def countCode(str):
-  len = str.length();
-
-  count = 0;
-
-  co = "co";
-
-  e = "e";
-
-   
-
-  if (len < 4):
-
-    return 0;
-
-   
-
-  for (i = 0, i < len - 3, i++):
-
-    if 
-        (co.compareTo(str.substring(i,i+2)) == 0 &&          e.compareTo(str.substring(i+3, i+4)) == 0)
-
-      count++;
-
-print countcode('aaacodebbb')
-
-  }
-
-  return count;
-
-}
+print Y
